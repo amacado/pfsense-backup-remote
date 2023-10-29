e57a44b400efe9f8cfa08608e8e8382dc602c301
amacado <8781699+amacado@users.noreply.github.com>
2023-10-29 02:45:10 +0200

    fix healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,6 +2,8 @@
 source "/run/scripts/functions.sh"
 
 glcoud_auth
-gcloud auth list --filter=status:ACTIVE --format="value(account)"
+/var/lib/google-cloud-sdk/bin/gcloud auth list --filter=status:ACTIVE --format="value(account)" || exit 1
 
 # @todo: add health checks
+
+exit 0
